331ae719a00c4ee9d545f0485529520e270d6f08
XaviPZ <pertuzx728@gmail.com>
2024-09-13 15:12:27 -0500

    Insigth
    Modificacion del Insigth

--- src/components/index/InsightItem.jsx
+++ src/components/index/InsightItem.jsx
@@ -1,66 +1,42 @@
 export default function InsightItem(data) {
     console.log(data)
     return (
-        <div class="views-row">
-                        <a href="youth-leadership-climate-action-shaping-sustainable-future.html" rel="bookmark">
-                            <article role="article" class="node node--type-resource node--view-mode-card card-body">
-                                <div class="node__content">
-                                    <div class="img-wrapper">
-                                        <div class="img">
-                                            <div
-                                                class="field field--name-field-media field--type-entity-reference field--label-hidden field__item"
-                                            >
-                                                <article
-                                                    class="media media--type-image media--view-mode-card"
-                                                >
-                                                    <div class="field field--name-field-media-image field--type-image field--label-visually_hidden">
-                                                        <div class="field__label visually-hidden">
-                                                            Image
-                                                        </div>
-                                                        <div class="field__item">
-                                                            <img
-                                                                loading="lazy"
-                                                                src={data.data.image}
-                                                                width="621"
-                                                                height="348"
-                                                                alt={data.data.subtitle}
-                                                                class="image-style-card"
-                                                            />
-                                                        </div>
-                                                    </div>
-                                                </article>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="info">
-                                        <div>
-                                            <div
-                                                class="field field--name-field-category field--type-entity-reference field--label-hidden field__items"
-                                            >
-                                                <div class="field__item">
-                                                    {data.data.title}
-                                                </div>
-                                            </div>
-
-                                            <h2>
-                                                <span class="field field--name-title field--type-string field--label-hidden"> 
-                                                    {data.data.subtitle}
-                                                </span>
-                                            </h2>
-
-                                            <div
-                                                class="field field--name-body field--type-text-with-summary field--label-hidden field__item"
-                                            >
-                                                <p>
-                                                    {data.data.description}
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="read-more">Read more</div>
-                                    </div>
+        <div class="field__item hover:border-blue-400 hover:border-1 hover:shadow-lg hover:shadow-[0_0_15px_5px_rgba(96,165,250,0.4)]">
+            <div>
+                <a href="services.html">
+                    <div>
+                        <article>
+                            <div>
+                                <div class="field__item">
+                                    <img
+                                        loading="lazy"
+                                        src={data.data.image}
+                                        width="328"
+                                        height="184"
+                                        alt={data.data.subtitle}
+                                        class="w-full h-48 object-cover"
+                                    />
                                 </div>
-                            </article>
-                        </a>
+                            </div>
+                        </article>
                     </div>
+
+                    <div class="bottom-2 left-2 text-sky-400 text-sm font-semibold px-3 py-3 rounded">
+                        {data.data.title}
+                    </div>
+                    <h2 class="text-xl font-bold px-3 py-1">
+                        <span>
+                            {data.data.subtitle}
+                        </span>
+                    </h2>
+                    <div class="text-sm mt-2 ml-2 py-1">
+                        <p>
+                            {data.data.description}
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <a class="underline underline-offset-4 m-2 text-orange-500 font-bold py-4">Read more</a>
+        </div>
     )
 }
